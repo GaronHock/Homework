f08999e2c6e4bac11efec772877546f8deebf034
Garon Hock <garonhock@yahoo.com>
2019-11-18 09:11:05 -0500

    queue class added

--- W6D1 /a_stack.rb
+++ W6D1 /a_stack.rb
@@ -16,3 +16,23 @@
       puts my_array.last
     end
   end
+
+  class Queue 
+
+    attr_reader :my_queue
+    
+    def initialize
+        @my_queue = []
+    end
+    def enqueue(el)
+        my_queue.unshift(el)
+    end
+
+    def dequeue 
+        my_queue.pop
+    end
+
+    def peek 
+        puts my_queue.last
+    end
+end
