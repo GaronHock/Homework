f73d126970b20439a780bb85f707a876d43a8ea6
Garon Hock <garonhock@yahoo.com>
2020-04-21 16:27:32 -0400

    adds coding challenges

--- CodingChallenges/challenges.js
+++ CodingChallenges/challenges.js
@@ -647,6 +647,23 @@ WordSplit(["hellocat", "hell,apple,bat,cat,goodbye,hello,yellow,why"])
         comp[target-nums[i]] = i      
     }
 };
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+    const twoSum = function(nums, target) {
+    const comp = {};
+    for (let i=0; i<nums.length; i++){
+        if (nums[i] in comp){          
+            return [ comp[nums[i] ] , i]
+        }
+        comp[target-nums[i]] = i      
+    }
+};
+
+
+
 
 
 
