118f1b5c219d65a79832bf1aa0afc782c90a1ef4
Garon Hock <garonhock@yahoo.com>
2020-04-29 21:36:55 -0400

    adds superincreasing solution

--- CodingChallenges/challenges.js
+++ CodingChallenges/challenges.js
@@ -1180,6 +1180,18 @@ function GCF(arr) {
 }
    
 
+function Superincreasing(arr) { 
+  let sum = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i] > sum) sum += arr[i]
+    else{
+      return false;
+    }
+  }
+
+  return true;
+
+}
 
 
 
