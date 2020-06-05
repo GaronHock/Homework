62f4f3a9349554f319f19d4047295a5697bfc79f
Garon Hock <garonhock@yahoo.com>
2020-06-05 13:02:54 -0400

    adds code minification

--- AngularUdemyCourse/OtherResources/Starter/app.js
+++ AngularUdemyCourse/OtherResources/Starter/app.js
@@ -1,5 +1,20 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope) {
-    
+myApp.controller('mainController', function($scope , $log, $filter, $resource) {
+
+  // console.log($log);
+  // $log.log("Hello");
+  // //$log.info("Info");
+  // $log.debug("debug info");
+  // $log.warn("This is a warning");
+  // $log.error("Throw Error")
+  // console.log($filter);
+
+
+  // $scope.name = "Garon";
+  // $scope.filteredText = $filter('uppercase')($scope.name);
+  // console.log($scope.filteredText);
+
+  console.log($resource);
+ // console.log($error);
 });
