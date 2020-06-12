6b595cf83b8f3d89f5b812f094a226b334664c8c
Garon Hock <garonhock@yahoo.com>
2020-06-12 13:56:24 -0400

    adds more files

--- AngularUdemyCourse/CommonDirectives/Starter/app.js
+++ AngularUdemyCourse/CommonDirectives/Starter/app.js
@@ -2,10 +2,31 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
     
-    $scope.handle = '';
-    
-    $scope.lowercasehandle = function() {
-        return $filter('lowercase')($scope.handle);
-    };
-    
+    // $scope.handle = '';
+    // $scope.characters = 5;
+    // $scope.lowercasehandle = function() {
+    //     return $filter('lowercase')($scope.handle);
+    // };
+
+
+    $scope.clicked = false;
+    $scope.rules = [
+
+        {rulename: 'Must be 5 characters'},
+        {rulename: 'Must not be used elsewhere'},
+        {rulename: 'Must be cool'}
+    ]
+
+    $scope.count = 0;
+    $scope.alertClick = () => {
+        if ($scope.count % 2 === 0){
+            $scope.clicked = true;
+        } else {
+            $scope.clicked = false;
+        }
+        $scope.count += 1;
+    }
+
+
+    //there is more than ng-click, like ng-keydown, ng-change/  can respond to all normal events
 }]);
