61815a341dd0f4c2e641b633e6fd23bb05e1a460
Garon Hock <garonhock@yahoo.com>
2020-06-12 14:29:35 -0400

    adds externalDataHttp request code

--- AngularUdemyCourse/ExternalDataHttp/Starter/app.js
+++ AngularUdemyCourse/ExternalDataHttp/Starter/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
     $scope.handle = '';
 
@@ -10,15 +10,36 @@ myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filt
 
     $scope.characters = 5;
 
-    var rulesrequest = new XMLHttpRequest();
-    rulesrequest.onreadystatechange = function () {
-        $scope.$apply(function () {
-            if (rulesrequest.readyState == 4 && rulesrequest.status == 200) {
-                $scope.rules = JSON.parse(rulesrequest.responseText);
-            }
-        });
-    }
-    rulesrequest.open("GET", "http://localhost:54765/api", true);
-    rulesrequest.send();
+    // var rulesrequest = new XMLHttpRequest();
+    // rulesrequest.onreadystatechange = function () {
+    //     $scope.$apply(function () {
+    //         if (rulesrequest.readyState == 4 && rulesrequest.status == 200) {
+    //             $scope.rules = JSON.parse(rulesrequest.responseText);
+    //         }
+    //     });
+    // }
+    // rulesrequest.open("GET", "http://localhost:54765/api", true);
+    // rulesrequest.send();
 
+    $http.get('/api/')
+    .success((result) => {
+        $scope.rules = result;
+    })
+    .error((data, status) => {
+        console.log(data);
+    })
+
+
+    $scope.newRule = '';
+
+    $scope.addRule = () => {
+        $http.post('/api', {newRule: $scope.newRule})
+        .success((result) => {
+            $scope.rules = result;
+            $scope.newRule = '';
+        })
+        .error((data, status) => {
+            console.log(data);
+        })
+    };
 }]);
